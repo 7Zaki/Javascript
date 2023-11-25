0c4fa667437e2eabb634f4b7ee5b736b9ce66029
Mohammad Zaki Naeem <68181491+7Zaki@users.noreply.github.com>
2023-11-25 05:56:38 +0000

    Config changes

---
+++ .devcontainer/Dot_notation/notation.js
@@ -0,0 +1,8 @@
+let profile = {
+    name: "Zaki",
+    age: 25
+}
+let a = "name";
+console.log(profile["a"];
+console.log(profile.name(a);
+console.log(profile.name.a
--- .devcontainer/basics/firstcode.js
+++ .devcontainer/basics/firstcode.js
@@ -1,1 +1,2 @@
 console.log("Zaki");
+console.log("Bilal");
