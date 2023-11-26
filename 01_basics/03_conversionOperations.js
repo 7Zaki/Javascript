f436db763af5ec722f52347e00d76d6c8fda6e06
Mohammad Zaki Naeem <68181491+7Zaki@users.noreply.github.com>
2023-11-26 09:41:53 +0000

    conversion operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -85,9 +85,50 @@ let stringSomeNumber = String(someNumber)
 console.log(typeof stringSomeNumber);
 
 
+// **************************Operations********************************
 
+let value = 3
+let negValue = -value 
+console.log(negValue);
 
+// console.log(2+2) addition
+// console.log(2-2) substraction
+// console.log(2*2) multiply
+// console.log(2**3) to the power
+// console.log(2/3) divide quotient
+// console.log(2%3) divide remainder
 
+let str1 = "hello"
+let str2 = " Zaki"
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2");
+
+console.log(true)
+console.log(+true)
+
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++ 
+console.log(gameCounter);
+++gameCounter
+console.log((gameCounter));
+
+// Prefix and postfix increment
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let x = 3
+const y = x++
+console.log(y);
+
+let xx = 3
+const yy = ++xx 
+console.log(yy);
 
 
 
