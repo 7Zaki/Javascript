0304dff1b456543fa3ee8e5140ebf1713e83ab8b
Mohammad Zaki Naeem <68181491+7Zaki@users.noreply.github.com>
2023-12-01 02:55:22 +0000

    Numbers

---
+++ 01_basics/08_numbers.js
@@ -0,0 +1,21 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(600)
+console.log(balance);
+
+const newBalance = balance.toString()
+console.log(newBalance);
+console.log(newBalance.length);
+console.log(typeof newBalance);
+console.log(balance.toFixed(2));
+
+const otherNumber = 1223.89666
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000000
+const sortHundreds = hundreds.toLocaleString('en-IN')
+console.log(sortHundreds);
+
+
+
