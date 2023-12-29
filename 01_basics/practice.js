d27b0d1fd531ad4912bbc07b9366b57a02c432de
Mohammad Zaki Naeem <68181491+7Zaki@users.noreply.github.com>
2023-12-29 07:06:49 +0000

    Practice

---
+++ 01_basics/practice.js
@@ -0,0 +1,65 @@
+/* const num = 400
+console.log(num);
+
+const numb = Number(400)
+console.log(numb.toFixed(1));
+
+const numbe = new Number(400)
+const number = numbe.toString();
+console.log(typeof number);
+
+console.log(num.toFixed(5));
+
+const digi = 12345.49230
+const newDigi = String(digi)
+const updatedDigi = new Number(digi)
+
+console.log(typeof digi);
+console.log(typeof newDigi);
+console.log(typeof updatedDigi);
+
+console.log(digi.toPrecision(5));
+
+const hundres = 100000000
+console.log(hundres.toLocaleString('en-IN'));
+
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.2));
+console.log(Math.round(4.6));
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.2));
+
+console.log(Math.pow(2,4));
+console.log(Math.min(2,3,4,4,5));
+console.log(Math.max(3,3,4,6,6,6,7,7,8,8,));
+ */
+
+
+//onsole.log(Math.floor((Math.random()*10)) + 1);
+
+const min = 10
+const max = 60
+
+console.log(Math.floor(Math.random() * ((max - min)+1)) + min);
+
+
+console.log(typeof null);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
