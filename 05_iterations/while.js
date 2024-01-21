b11c5f07c1652ac3dde669ec5cac8418e7a03254
Mohammad Zaki Naeem <68181491+7Zaki@users.noreply.github.com>
2024-01-21 06:23:27 +0000

    while and do while loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,24 @@
+
+let index = 0
+while (index <= 10) {
+    console.log(`value of i is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(`value of index is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+let score = 12
+
+do {
+    console.log(`value of score is ${score}`);
+    score++
+} while (score < 10);
