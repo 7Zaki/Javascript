cea49032f5913ef863f727197092f333f38af1c3
Mohammad Zaki Naeem <68181491+7Zaki@users.noreply.github.com>
2024-01-28 11:40:22 +0000

    Conclusion

---
+++ 05_iterations/conclusions.js
@@ -0,0 +1,17 @@
+// For-of loop works on arrays, strings and maps. it does not work on objects
+
+//For-in loop works on both arrays and objects but it does not work on maps
+//For-in loop always throws up keys of both arrays and objects
+
+//forEach loop works best with arrays or an array of objects
+
+const coding = ["java", "javascript", "cpp", "go", "flutter"]
+
+coding.forEach( (i, j, k) => {
+    console.log(i, j, k)
+})
+
+
+
+
+
